84940ce70ed89fb75d7b9dc63f6f956ac060d49d
chenhongyu <chenhongyu@qianxiao.local>
2016-08-04 17:02:02 +0800

    Initial Commit

---
+++ Block循环引用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Block循环引用
+//
+//  Created by ~~浅笑 on 16/8/4.
+//  Copyright © 2016年 广东康爱多连锁药店有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
